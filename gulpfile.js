3c93c75dccad0b3edf024978302c78097da1c1e0
MaryoryChavez <maryory.chavez@gmail.com>
2017-03-28 18:31:37 -0500

    dist-src

--- gulpfile.js
+++ gulpfile.js
@@ -6,49 +6,68 @@ var notify = require('gulp-notify');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
+var minify = require('gulp-minify');
+
 
 
 gulp.task('script', function(cb){
+    /*gulp.src('./src/js/custom.js')
+    .pipe(gulp.dest('./dist/js'));*/
+    gulp.src('./src/js/ciudades.js')
+    .pipe(gulp.dest('./dist/js'));
 	// tarea script
 });
 
+ 
+gulp.task('compress', function() {
+  gulp.src('./src/js/*.js')
+    .pipe(minify({
+        ext:{
+            min:'.min.js'
+        },
+        exclude: ['ciudades.js'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('./dist/js'))
+});
+
 gulp.task('style', function() {
-    return gulp.src('./dist/sass/*.scss')
+    return gulp.src('./src/sass/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./dist/css'));
 	// tarea style
 });
 
 gulp.task('boostrap',function(){
    gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
-    .pipe(gulp.dest('./src/css')); 
+    .pipe(gulp.dest('./dist/css')); 
     gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(gulp.dest('./src/js'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('fonts',function(){
    gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
-   .pipe(gulp.dest('./src/css'));
+   .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('jq',function(){
     gulp.src('./node_modules/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('./src/js'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('images', function() {
     gulp.src('./docs/*.jpg')
-    .pipe(gulp.dest('./src/img'));
+    .pipe(gulp.dest('./dist/img'));
 	// tarea images
 });
 
 gulp.task('alert',function(){
     gulp.src('./node_modules/sweetalert/dist/sweetalert.min.js')
-    .pipe(gulp.dest('./src/js'));
+    .pipe(gulp.dest('./dist/js'));
     
     gulp.src('./node_modules/sweetalert/dist/sweetalert.css')
-    .pipe(gulp.dest('./src/css')); 
+    .pipe(gulp.dest('./dist/css')); 
 })
 
 
-gulp.task('default', ['images', 'style','boostrap', 'script','fonts','jq','alert']);
+gulp.task('default', ['images', 'style','boostrap', 'script','fonts','jq','alert','compress']);
